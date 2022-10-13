e413c23ad761ccadd15ab37268c2a44c5e53d047
marlodycairo <45442612+marlodycairo@users.noreply.github.com>
2022-10-13 06:47:21 -0500

    Initial commit

---
+++ TecnicalTestLibrary.Api.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TecnicalTestLibrary.Api.Infrastructure.Entities;
+
+namespace TecnicalTestLibrary.Api.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
